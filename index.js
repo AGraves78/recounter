f3c5157da70103d7606a6994cacfdb0970e95c54
Angela Graves <algraves78@gmail.com>
2017-06-06 15:56:26 -0400

    created initial route and configured the static serving of html, js, and css

--- index.js
+++ index.js
@@ -9,5 +9,5 @@ server.get('/recounter', (request, response) => {
   response.sendFile('public/html/index.html', {root: __dirname});
 });
 server.listen(port, () => {
-  console.log('Now listening on port ', port);
+  console.log('Now listening on port', port);
 });
